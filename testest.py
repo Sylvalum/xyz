794719da9d3f4f6d258f9e77789de4c263422841
Nicolás Tobalina <nicolas.tobalina@samelan.com>
2019-04-25 12:32:19 +0200

    asdf

--- testest.py
+++ testest.py
@@ -1,5 +1,5 @@
 from bottle import Bottle, route, run, template
-import os
+import os, time
 
 app = Bottle()
 
@@ -14,6 +14,11 @@ def sub(num1, num2):
     total = str(int(num1) - int(num2))
     print(total)
     return template('<b>{{total}}</b>', total=total)
+	
+@app.get('/asdf')
+def asdf():
+	time.sleep(10)
+	return 'hola'
 
 port = os.environ.get('PORT', 5000)
 run(app, host='0.0.0.0', port=port)
